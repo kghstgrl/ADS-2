45345985d5f9a109444a0e15a5bdd6d58f0c680f
kghstgrl <160274071+kghstgrl@users.noreply.github.com>
2024-03-11 01:42:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -42,8 +42,15 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
     double sum = 1.0;
+    double ns = -1.0;
     for (uint16_t i = 1; i <= count; i++) {
-        sum += ((i % 2 == 0) ? 1 : -1) * pown(x, 2 * i) / fact(2 * i);
+        double result = ns * pown(x, 2 * i) / fact(2 * i);
+        sum = sum + result;
+        ns *= -1.0;
+        double sum = 0;
+    }
+    for (uint64_t i = 1; i <= count; i++) {
+        sum += calcItem(x, (2 * i) - 2) * pown(-1, i-1);
     }
     return sum;
 }
